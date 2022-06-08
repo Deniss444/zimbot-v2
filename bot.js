5eaac99a4678015c604973f498f014fa30bab88f
Deniss444 <69092379+Deniss444@users.noreply.github.com>
2022-06-08 13:51:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,15 +33,15 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.owner = ['2347051364291',''] //owner number +27634090203
+global.pemilik = ['2347051364291'] //owner number +27634090203
+global.premium = ['2347051364291'] //premium number +27634090203
+global.pengguna = 'Dennis' //username
+global.footer = 'ZADDYS TECH BOT INC'
+global.botnma = 'ZADDYS TECH BOT INC' //bot name
+global.ownernma = 'Dennis' //owner name
+global.packname = 'Zaddys Tech⚕' //sticker package name
+global.author = 'Dennis🈹️' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
